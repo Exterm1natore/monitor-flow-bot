23e336c91351406696dbbc08a41b10bc453efdab
Ilya <131213142+Exterm1natore@users.noreply.github.com>
2025-05-05 01:07:01 +0300

    feat: add search for unscripted chat notifications

--- app/db/crud/notification_types.py
+++ app/db/crud/notification_types.py
@@ -1,6 +1,7 @@
+from typing import List
 from sqlalchemy.orm import Session
 from sqlalchemy import select
-from app.db.models import NotificationType
+from app.db.models import NotificationType, Chat, NotificationSubscriber
 from typing import Optional, Union
 
 
@@ -22,3 +23,23 @@ def find_notification_type(db: Session, identifier: Union[int, str]) -> Optional
         raise TypeError(f"❌ Identifier must be of type int or str, received: {type(identifier).__name__}")
 
     return db.execute(stmt).scalar_one_or_none()
+
+
+def find_unsubscribed_notification_types(db: Session, chat: Chat) -> List[NotificationType]:
+    """
+    Найти список типов уведомлений, на которые чат не подписан.
+
+    :param db: Сессия базы данных.
+    :param chat: Чат, для которого ищутся неподписанные типы уведомлений.
+    :return: Список типов уведомлений, на которые чат не подписан.
+    """
+    if chat is None:
+        return []
+
+    # Под-запрос для получения всех ID типов, на которые чат уже подписан
+    subquery = select(NotificationSubscriber.notification_type).where(NotificationSubscriber.chat_id == chat.id).subquery()
+
+    # Основной запрос: все NotificationType, которых нет в подписках
+    stmt = select(NotificationType).where(NotificationType.id.not_in(select(subquery)))
+
+    return db.execute(stmt).scalars().all()

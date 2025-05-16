77e992a97e0c13000e70b63c0439c44db9147bc0
Ilya <131213142+Exterm1natore@users.noreply.github.com>
2025-05-17 01:16:56 +0300

    feat: remove delay in sending messages to chats

--- app/core/bot_extensions/messages.py
+++ app/core/bot_extensions/messages.py
@@ -12,8 +12,6 @@ def send_text_to_chats(
         parse_mode=None,
         format_=None,
         max_workers: int = 5,
-        pause_every: int = 20,
-        pause_seconds: float = 2.0,
         wait_for_completion: bool = False
 ):
     """
@@ -24,10 +22,8 @@ def send_text_to_chats(
     :param text: Текст сообщения.
     :param inline_keyboard_markup: Встроенная в сообщение клавиатура.
     :param parse_mode: Тип разбора текста.
-    :param format_: Формат текста.
+    :param format_: Описание форматирования текста.
     :param max_workers: Число потоков в пуле.
-    :param pause_every: Делать паузу после каждого N-го submit.
-    :param pause_seconds: Длительность паузы.
     :param wait_for_completion: Дождаться завершения всех задач.
     """
     def safe_send(chat_id):
@@ -45,7 +41,5 @@ def send_text_to_chats(
     executor = ThreadPoolExecutor(max_workers=max_workers)
     for i, item_chat_id in enumerate(chat_ids, start=1):
         executor.submit(safe_send, item_chat_id)
-        if pause_every and i % pause_every == 0:
-            time.sleep(pause_seconds)
 
     executor.shutdown(wait=wait_for_completion)

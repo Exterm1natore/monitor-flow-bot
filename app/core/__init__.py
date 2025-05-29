a1f79c217dde33ee292e4423c8dd9afdef540ef4
Ilya <131213142+Exterm1natore@users.noreply.github.com>
2025-05-29 14:02:25 +0300

    refactor: remove unnecessary imports in __init__.py

--- app/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import environment
-from . import bot_setup
-from . import bot_extensions
-from . import executor_pool
--- app/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import date_and_time
-from . import text_format
-from . import db_records_format
-from . import json_format
